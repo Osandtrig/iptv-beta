e2c5b2482a9ef1bd0662de17c17792ccb555ba53
Osandtrig <124008783+Osandtrig@users.noreply.github.com>
2026-03-08 20:23:37 -0300

    Update controller.js

--- js/controller.js
+++ js/controller.js
@@ -1,1 +1,20 @@
 
+window.onload = function(){
+
+const channelList = document.getElementById("channelList");
+
+channels.forEach(function(channel){
+
+let button = document.createElement("button");
+
+button.innerText = channel.name;
+
+button.onclick = function(){
+playChannel(channel.stream);
+};
+
+channelList.appendChild(button);
+
+});
+
+};

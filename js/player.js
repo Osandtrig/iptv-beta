d57ac687ba2e13f8914859055ddcc6374cf8941f
Osandtrig <124008783+Osandtrig@users.noreply.github.com>
2026-03-08 20:43:53 -0300

    Update player.js

--- js/player.js
+++ js/player.js
@@ -1,13 +1,27 @@
 
-var player = videojs('player');
-
 function playChannel(url){
 
-player.src({
-src: url,
-type: "application/x-mpegURL"
+const video = document.getElementById("videoPlayer");
+
+if(Hls.isSupported()){
+
+const hls = new Hls();
+hls.loadSource(url);
+hls.attachMedia(video);
+
+hls.on(Hls.Events.MANIFEST_PARSED,function(){
+video.play();
 });
 
-player.play();
+}
+
+else if(video.canPlayType("application/vnd.apple.mpegurl")){
+
+video.src = url;
+video.addEventListener("loadedmetadata",function(){
+video.play();
+});
+
+}
 
 }

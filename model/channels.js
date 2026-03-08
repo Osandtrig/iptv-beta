3534c6e8e62aa427f4ac03901ba2ad629ebfd5ce
Osandtrig <124008783+Osandtrig@users.noreply.github.com>
2026-03-08 20:29:46 -0300

    Update channels.js

--- model/channels.js
+++ model/channels.js
@@ -2,11 +2,13 @@ const channels = [
 
 {
 name: "Canal Demo",
+logo: "https://upload.wikimedia.org/wikipedia/commons/3/3f/Logo_Test.png",
 stream: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
 },
 
 {
 name: "Canal Test 2",
+logo: "https://upload.wikimedia.org/wikipedia/commons/3/3f/Logo_Test.png",
 stream: "https://test-streams.mux.dev/test_001/stream.m3u8"
 }
 

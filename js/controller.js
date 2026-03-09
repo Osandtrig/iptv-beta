a2efa399b65a6aa7305d5abfaa5f0e0bde05687f
Osandtrig <124008783+Osandtrig@users.noreply.github.com>
2026-03-08 21:54:48 -0300

    Update controller.js

--- js/controller.js
+++ js/controller.js
@@ -1,23 +1,58 @@
 
-window.onload = function(){
+async function loadPlaylist(){
+
+const url = document.getElementById("m3uUrl").value;
+
+const response = await fetch(url);
+const text = await response.text();
+
+parseM3U(text);
+
+}
+
+function parseM3U(data){
+
+channels = [];
+
+const lines = data.split("\n");
+
+for(let i=0;i<lines.length;i++){
+
+if(lines[i].startsWith("#EXTINF")){
+
+let name = lines[i].split(",")[1];
+let stream = lines[i+1];
+
+channels.push({
+name:name,
+logo:"",
+stream:stream
+});
+
+}
+
+}
+
+renderChannels();
+
+}
+
+function renderChannels(){
 
 const list = document.getElementById("channelList");
+list.innerHTML="";
 
-channels.forEach(channel => {
+channels.forEach(channel=>{
 
-let card = document.createElement("div");
-card.className = "channel-card";
+let card=document.createElement("div");
+card.className="channel-card";
 
-let img = document.createElement("img");
-img.src = channel.logo;
+let name=document.createElement("p");
+name.innerText=channel.name;
 
-let name = document.createElement("p");
-name.innerText = channel.name;
-
-card.appendChild(img);
 card.appendChild(name);
 
-card.onclick = () => playChannel(channel.stream);
+card.onclick=()=>playChannel(channel.stream);
 
 list.appendChild(card);
 

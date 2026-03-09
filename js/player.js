8f21a4bf305b3b96f00d8f4ebd38ab2a0d49037b
Osandtrig <124008783+Osandtrig@users.noreply.github.com>
2026-03-09 00:00:21 -0300

    Update player.js

--- js/player.js
+++ js/player.js
@@ -1,32 +1,22 @@
 
 function playChannel(url){
+    const video = document.getElementById("videoPlayer");
+    if(!video) return console.error("Video player no encontrado");
 
-const video = document.getElementById("videoPlayer");
+    video.pause();
+    video.src = ""; // limpiar
 
-if(!video){
-console.error("Video player no encontrado");
-return;
-}
-
-if(Hls.isSupported()){
-
-const hls = new Hls();
-hls.loadSource(url);
-hls.attachMedia(video);
-
-hls.on(Hls.Events.MANIFEST_PARSED,function(){
-video.play();
-});
-
-}
-
-else if(video.canPlayType("application/vnd.apple.mpegurl")){
-
-video.src = url;
-video.addEventListener("loadedmetadata",function(){
-video.play();
-});
-
-}
-
+    if(Hls.isSupported() && url.endsWith(".m3u8")){
+        const hls = new Hls();
+        hls.loadSource(url);
+        hls.attachMedia(video);
+        hls.on(Hls.Events.MANIFEST_PARSED, ()=> video.play());
+    } else if(url.endsWith(".mpd")){
+        const player = new shaka.Player(video);
+        player.load(url).then(()=> video.play())
+        .catch(e=> console.error("Error DASH:", e));
+    } else {
+        video.src = url;
+        video.play().catch(()=> console.warn("No se pudo reproducir el video"));
+    }
 }
